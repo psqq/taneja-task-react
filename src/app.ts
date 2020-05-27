23da1b34a871a9d549f78d9cdc11ff9307a7d969
psqq <pestov-sa@ya.ru>
2020-05-28 01:42:42 +0300

    No app is module - not class

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,30 @@
 import * as reactApp from './react-app';
 
-class App {
-
-    // Public section
-    public noCordova() {
-        this.cordova = false;
-    }
-    public main() {
-        if (this.running) {
-            throw "The application is already running.";
-        }
-        this.running = true;
-        if (this.cordova) {
-            document.addEventListener('deviceready', () => this.onDeviceReady(), false);
-        } else {
-            this.onDeviceReady();
-        }
-    }
-
-    // Private section
-    private cordova: boolean = true;
-    private running: boolean = false;
-    private run() {
-        reactApp.main();
-    }
-    private onDeviceReady() {
-        this.run();
-    }
-
+// Public section
+export function noCordova() {
+    cordova = false;
 }
 
-const app = new App();
+export function main() {
+    if (running) {
+        throw "The application is already running.";
+    }
+    running = true;
+    if (cordova) {
+        document.addEventListener('deviceready', onDeviceReady, false);
+    } else {
+        onDeviceReady();
+    }
+}
 
-export default app;
+// Private section
+let cordova: boolean = true;
+let running: boolean = false;
+
+function run() {
+    reactApp.main();
+}
+
+function onDeviceReady() {
+    run();
+}
--- src/index.cordova.ts
+++ src/index.cordova.ts
@@ -1,3 +1,3 @@
-import app from "./app";
+import * as app from "./app";
 
 app.main();
